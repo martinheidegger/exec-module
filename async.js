217ac04d3bd1d7ad85c3c5b22b75f99b24e17bad
Martin Heidegger <martin.heidegger@gmail.com>
2016-10-27 01:52:22 +0900

    Added access check

--- async.js
+++ async.js
@@ -34,43 +34,48 @@ module.exports = function (file, opt, callback) {
     if (stat && !stat.isFile()) {
       return callback(createErrorTemplate(file, 'STAT_NOFILE', 'Path is not a file'))
     }
-    try {
-      var mod = require(file)
-    } catch (reqErr) {
-      return callback(createErrorTemplate(file, 'LOAD', 'Error while loading file', reqErr))
-    }
-    if (typeof mod !== 'function') {
-      return tearDown(createErrorTemplate(file, 'FUNCTION_WRONG', 'Function needs to be a function.'))
-    }
-    if (opt.argCount !== undefined && mod.length !== opt.argCount) {
-      var argErr = createErrorTemplate(file, 'FUNCTION_ARG_WRONG', 'Function argument count mismatch.')
-      argErr.expected = opt.argCount
-      argErr.actual = mod.length
-      return tearDown(argErr)
-    }
-    var run = function (err, result) {
-      if (err) {
-        return callback(err)
+    fs.access(file, fs.constants.R_OK, function (accessErr) {
+      if (accessErr) {
+        return callback(createErrorTemplate(file, 'CANT_READ', 'File is not readable', accessErr))
       }
-      execAsync({
-        name: 'run',
-        run: opt.exec || function (file, opt, mod, callback) {
-          mod(callback)
-        },
-        args: [file, opt, mod],
-        file: file,
-        timeout: opt.timeout,
-        then: tearDown
-      })
-    }
+      try {
+        var mod = require(file)
+      } catch (reqErr) {
+        return callback(createErrorTemplate(file, 'LOAD', 'Error while loading file', reqErr))
+      }
+      if (typeof mod !== 'function') {
+        return callback(createErrorTemplate(file, 'FUNCTION_WRONG', 'Function needs to be a function.'))
+      }
+      if (opt.argCount !== undefined && mod.length !== opt.argCount) {
+        var argErr = createErrorTemplate(file, 'FUNCTION_ARG_WRONG', 'Function argument count mismatch.')
+        argErr.expected = opt.argCount
+        argErr.actual = mod.length
+        return callback(argErr)
+      }
+      var run = function (err, result) {
+        if (err) {
+          return callback(err)
+        }
+        execAsync({
+          name: 'run',
+          run: opt.exec || function (file, opt, mod, callback) {
+            mod(callback)
+          },
+          args: [file, opt, mod],
+          file: file,
+          timeout: opt.timeout,
+          then: tearDown
+        })
+      }
 
-    execAsync({
-      name: 'setUp',
-      run: opt.setUp,
-      allowEmpty: true,
-      args: [file, opt],
-      file: file,
-      then: run
+      execAsync({
+        name: 'setUp',
+        run: opt.setUp,
+        allowEmpty: true,
+        args: [file, opt],
+        file: file,
+        then: run
+      })
     })
   })
 }
